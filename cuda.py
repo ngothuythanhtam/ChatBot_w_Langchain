32b00ae1c2e15e82123fb074897717c23ff5acea
ngothuythanhtam <ntthanhtam.forwork@gmail.com>
2024-11-16 23:40:32 +0700

    Update

--- cuda.py
+++ cuda.py
@@ -1,8 +1,4 @@
 import torch
 import os
-
-# Check if GPU is available
 print("Is CUDA available? ", torch.cuda.is_available())
-
-# Run nvidia-smi command from Python and print the output
 os.system("nvidia-smi")
